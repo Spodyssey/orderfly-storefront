dbf773890c84ce7c2f12a415ba4151228dc75426
Zackery Bednar <spodesy@gmail.com>
2023-06-16 18:44:35 -0400

    Updated return of read marketplace items

--- src/models/marketplace.py
+++ src/models/marketplace.py
@@ -1,5 +1,6 @@
 import logging, requests, sqlite3
 from bs4 import BeautifulSoup
+from models.item import Item
 
 from models.marketplaceItem import MarketplaceItem
 
@@ -90,13 +91,7 @@ class MarketplaceDAO:
         if rows:
             for row in rows:
                 item_asin, item_name, listing_url, first_seen, last_seen = row
-                marketplace_items.append({
-                    "item_name": item_name, 
-                    "item_asin": item_asin, 
-                    "listing_url": listing_url, 
-                    "first_seen": first_seen, 
-                    "last_seen": last_seen
-                })    
+                marketplace_items.append(Item(item_asin, item_name, listing_url, first_seen, last_seen))    
             return marketplace_items
         else:
             return None
